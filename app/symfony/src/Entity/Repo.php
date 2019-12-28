5d3ee9502a32b385353c85877e6d805624a08262
Kamil Purzynski <kamil.purzynski@gmail.com>
2019-12-28 14:52:05 +0100

    Prepared skeleton of app.

---
+++ app/symfony/src/Entity/Repo.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\RepostioryRepository")
+ * @ORM\Table(name="repository")
+ */
+class Repo
+{
+    /**
+     * @var UuidInterface
+     *
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    protected UuidInterface $id;
+
+    protected string $name;
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+}
