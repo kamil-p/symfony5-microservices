e87ea91ae726c799730ff59f132532bd9959b1db
Kamil Purzynski <kamil.purzynski@gmail.com>
2019-12-31 17:37:19 +0100

    Changed docker files structure and added iam application to the repo.

---
+++ iam/symfony/src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number/{max}", name="app_lucky_number")
+     */
+    public function number($max)
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
